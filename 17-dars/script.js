a285ba7f0c5755e375f780ab634672f9c70d2ccb
yulduzoromova <yulduzoromova1993@gmail.com>
2023-10-24 11:59:22 +0300

    Scope | Closure

--- 17-dars/script.js
+++ 17-dars/script.js
@@ -17,6 +17,28 @@ console.log(i);    // noto'g'ri natijani chiqarib qo'yishimiz mumkin.
 // - Environment Record - an object stores all local variables / local memory - 
 // - Reference to outer lexical environment - assosiated with outer code
 // - draw callstack 
+// leksik muhit 2 qismdan iborat bu 1- localniy o'zgaruvchilari va this kalit so'zi, 2-si esa o'zidan 1 pog'ana balandda joylashgan leksik muhit ssilkasi kiradi. Leksik muhit qachonki funksiya chaqirilganda yuzaga keladi.
+
+let userName = "Vataliy";
+
+const foo = () => {
+  let userName2 = "Vasiya";
+
+  console.log(userName + userName2);  // bu funksiyaning leksik muhitida userName2 bor ln userName yo'q. Agar funksiya chaqirilganda userName o'zgaruvchisini topolmasa u o'zidan bitta pog'ana tashqaridan qidiradi va topsa consolega shu qiymatni chiqaradi, topmaguncha 1ta pog'ona teppaga ko'tarilib qidiraveradi, topolmasa undefined qaytaradi. 
+
+}
+//=================
+
+let user = 'Pavel';
+
+const printName = () => {
+  console.log('Hello' + user);
+};
+
+user = "Vitaliy";
+
+printName()            // HelloVitaliy    // leksik muhit funksiya chaqirilganda yuzaga kelayapti, va funcsiya ichida user variableni topolmagani uchun funcsiya chaqirilgandan oldingi userni qiymatini olayapti.
+
 
 function getName(){
   let b = 'wba'
@@ -48,3 +70,6 @@ function outer() {
 
 // GARBAGE COLLECTION - lexical environment is removed after function call 
 // functionni ichida ochilgan o'zgaruvchi faqat shu functionni ichida ishlida uni tashqarisida unga acsess qilolmimiz. Shu funksiya chqirilganda ishlatiladi va o'chirib yuboriladi, ya'ni xotirada ko'p joy egallamidi.
+
+
+// https://www.youtube.com/watch?v=Rc49Z6CNJj0 - JavaScript. Замыкание. Лексическое окружение.
