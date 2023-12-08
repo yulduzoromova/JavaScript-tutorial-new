3ca1a65df4bad3fe243f028d4ee09cfb11520f12
yulduzoromova <yulduzoromova1993@gmail.com>
2023-12-08 11:10:59 +0300

    Modules

---
+++ 28-dars/script.js
@@ -0,0 +1,10 @@
+//========== Modules ===========
+
+// modulesdan maqsad index.js alohida modul, mock.js alohida modul qilib qulay qilib olish
+// fayllarga ajratish deyiladi.
+// mock.jsdagi faylni index.htmlda ishlatish uchun uni export qilib qo'yishimiz kk
+// index.htmlda ishlatish uchun esa import qilamiz
+// import {users} from './mock.js'
+// bitta moduleda 1 ta export default bo'lishi mumkin
+// export default bo'layotgan ma'lumotni importda istalgan nom bilan chaqirsak bo'ladi {} qavslarsiz
+// export ham export default ham qilmasdan faqat import va fayl nomi yozilsa bu avtomatik ravishda fayl yurg'izib beradi funksiyalarni chaqirish ham shart emas
